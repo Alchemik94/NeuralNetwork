1c84b1df2f8e3f6c94b2cb528afaecf5258664ed
Alchemik94 <alchemik94@gmail.com>
2014-05-28 18:34:09 +0200

    Tester first attempt

--- NeuralNetwork/Tester.h
+++ NeuralNetwork/Tester.h
@@ -2,15 +2,31 @@
 #define __TESTER__
 
 #include "NeuralNetwork.h"
+#include <vector>
 
 class Tester
 {
-	NeuralNetwork network;
+	NeuralNetwork<> network;
+
+	const long double epsilon = 0.0001;
 
 public:
-	static void Test()
+	Tester()
 	{
+		vector<int> structure;
+		structure.push_back(2);
+		structure.push_back(4);
+		structure.push_back(4);
+		structure.push_back(2);
+		network = NeuralNetwork<>(structure);
+	}
 	
+	void Test()
+	{
+		//first test is to perform xor operation
+
+
+
 	}
 
 };
