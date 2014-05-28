c95cb9106ee79d1c880f5fa0eaeba03e27b176b6
Alchemik94 <alchemik94@gmail.com>
2014-05-28 17:21:10 +0200

    Added parser class

---
+++ NeuralNetwork/Parser.h
@@ -0,0 +1,19 @@
+#ifndef __PARSER__
+#define __PARSER__
+
+#include <vector>
+
+using namespace std;
+
+template <typename FloatingNumber = long double>
+class Parser
+{
+
+	public:
+		static vector<FloatingNumber> Parse()
+		{
+
+		}
+};
+
+#endif
