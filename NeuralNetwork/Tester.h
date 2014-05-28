c91d271226f07c6e15b35f06ddfa20da2e3bf44e
Alchemik94 <alchemik94@gmail.com>
2014-05-28 19:00:23 +0200

    Larger test

--- NeuralNetwork/Tester.h
+++ NeuralNetwork/Tester.h
@@ -3,6 +3,10 @@
 
 #include "NeuralNetwork.h"
 #include <vector>
+#include <list>
+#include <iostream>
+
+using namespace std;
 
 class Tester
 {
@@ -17,11 +21,31 @@ public:
 		structure.push_back(2);
 		structure.push_back(4);
 		structure.push_back(4);
-		structure.push_back(2);
+		structure.push_back(1);
 		NeuralNetwork<> network(structure);
 
+		network.Save("net.net");
 
+		vector<long double> in, out;
+		list<pair<vector<long double>, vector<long double> > > teachingSet;
+		for (int i = 0; i < 2;++i)
+		for (int j = 0; j < 2; ++j)
+		{
+			in.clear();
+			out.clear();
+			in.push_back(i);
+			in.push_back(j);
+			out.push_back(((i == 1) && (j == 0)) || ((i == 0) && (j == 1)));
+			teachingSet.push_back(make_pair(in, out));
+		}
 
+		network.Teach(teachingSet,10);
+		network.Save("xor.net");
+
+		NeuralNetwork<long double> network2("xor.net");
+		out = network2.Use(in);
+		
+		cout << out[0] << "\n";
 	}
 
 };
