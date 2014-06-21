32498ad458c74f471700f0762e204fe596e8d81b
Alchemik94 <alchemik94@gmail.com>
2014-06-21 19:21:46 +0200

    All together

--- NeuralNetwork/Main.cpp
+++ NeuralNetwork/Main.cpp
@@ -17,13 +17,14 @@ class Program
 	int whenReport;
 	const enum Command
 	{
-		unknown,create, select, list, teach, save, epochs, report, from_file, help, quit
+		unknown,create, select, list, teach, save, epochs, report, from_directory, help, quit
 	};
 
-	vector<pair<string, NeuralNetwork<> > > base;
+	vector<pair<string, vector<NeuralNetwork<> > > > base;
 	Command cmd;
 	int tmp, selected;
 	string tmpstr;
+	vector<NeuralNetwork<> > tmp_nns;
 
 public:
 	void TeachingFront(const string& teachingFileName)
@@ -31,21 +32,21 @@ public:
 		Parser<> parser(windowWidth);
 		vector<NeuralNetwork<>::TeachingSet> teachingSet = parser.Parse(teachingFileName);
 
-		base[selected-1].second.Teach(teachingSet[0], numberOfEpochs, whenReport);
+		base[selected-1].second[0].Teach(teachingSet[0], numberOfEpochs, whenReport);
 	}
 	void TeachingMiddle(const string& teachingFileName)
 	{
 		Parser<> parser(windowWidth);
 		vector<NeuralNetwork<>::TeachingSet> teachingSet = parser.Parse(teachingFileName);
 
-		base[selected-1].second.Teach(teachingSet[1], numberOfEpochs, whenReport);
+		base[selected-1].second[1].Teach(teachingSet[1], numberOfEpochs, whenReport);
 	}
 	void TeachingBack(const string& teachingFileName)
 	{
 		Parser<> parser(windowWidth);
 		vector<NeuralNetwork<>::TeachingSet> teachingSet = parser.Parse(teachingFileName);
 
-		base[selected-1].second.Teach(teachingSet[2], numberOfEpochs, whenReport);
+		base[selected-1].second[2].Teach(teachingSet[2], numberOfEpochs, whenReport);
 	}
 
 	void Help()
@@ -62,20 +63,27 @@ public:
 		cout << "10. quit - closes program\n";
 	}
 
-	vector<int> GetStructure()
+	vector<NeuralNetwork<> > GetStructure()
 	{
-		cout << "Sequence ends with nonpositive number. Network has to have at least 3 layers.\n";
+		vector<NeuralNetwork<> > result;
+		cout << "Sequences end with nonpositive numbers. Network has to have at least 3 layers.\n";
 		int i;
 		vector<int> structure;
-		cout << ">> ";
-		cin >> i;
-		while (i > 0)
+		for (int j = 0; j<3 ; ++j)
 		{
-			structure.push_back(i);
+			cout << "Part " << j + 1 << " of 3:\n";
+			structure.clear();
 			cout << ">> ";
 			cin >> i;
+			while (i > 0)
+			{
+				structure.push_back(i);
+				cout << ">> ";
+				cin >> i;
+			}
+			result.push_back(NeuralNetwork<>(structure));
 		}
-		return structure;
+		return result;
 	}
 
 	string GetName()
@@ -89,33 +97,13 @@ public:
 
 	void Teach()
 	{
-		cout << "There are 3 options for teaching of the network:\n";
-		cout << "1. Recognizing the fronts of the proteins' secondary structure.\n";
-		cout << "2. Recognizing the middle of the proteins' secondary structure.\n";
-		cout << "3. Recognizing the backs of the proteins' secondary structure.\n";
-		cout << "Select one of them (as a number).\n";
-		int sel;
-		cout << ">> ";
-		cin >> sel;
 		cout << "Enter training file name.\n";
 		cout << ">> ";
 		string training;
 		cin >> training;
-		switch (sel)
-		{
-		case 1:
-			TeachingFront(training);
-			break;
-		case 2:
-			TeachingMiddle(training);
-			break;
-		case 3:
-			TeachingBack(training);
-			break;
-		default:
-			cout << "Error. Try again.\n";
-			break;
-		}
+		TeachingFront(training);
+		TeachingMiddle(training);
+		TeachingBack(training);
 	}
 
 	string GetFilename()
@@ -141,7 +129,7 @@ public:
 			switch (cmd)
 			{
 			case create:
-				base.push_back(make_pair(GetName(), NeuralNetwork<>(GetStructure())));
+				base.push_back(make_pair(GetName(), GetStructure()));
 				cout << "Network created.\n";
 				break;
 			case select:
@@ -158,7 +146,9 @@ public:
 				cout << "Selected network has been taught.\n";
 				break;
 			case save:
-				base[selected - 1].second.Save(GetFilename());
+				base[selected - 1].second[0].Save(".\\" + GetFilename() + "\\_front.net");
+				base[selected - 1].second[1].Save(".\\" + GetFilename() + "\\_middle.net");
+				base[selected - 1].second[2].Save(".\\" + GetFilename() + "\\_back.net");
 				cout << "Selected network has been saved.\n";
 				break;
 			case epochs:
@@ -173,12 +163,16 @@ public:
 				cin >> whenReport;
 				cout << "Set report to appear after every " << whenReport << " epochs.\n";
 				break;
-			case from_file:
+			case from_directory:
 				cout << "Enter name of network file.\n";
 				cout << ">> ";
 				cin >> tmpstr;
-				base.push_back(make_pair(tmpstr, NeuralNetwork<>(tmpstr)));
-				cout << "Added network from file.\n";
+				tmp_nns.clear();
+				tmp_nns.push_back(NeuralNetwork<>(".\\"+tmpstr+"\\_front.net"));
+				tmp_nns.push_back(NeuralNetwork<>(".\\" + tmpstr + "\\_middle.net"));
+				tmp_nns.push_back(NeuralNetwork<>(".\\" + tmpstr + "\\_back.net"));
+				base.push_back(make_pair(tmpstr, tmp_nns));
+				cout << "Added network from directory.\n";
 				break;
 			case help:
 				Help();
